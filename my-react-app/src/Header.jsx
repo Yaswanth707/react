3afb552c54db7f1e463ed37857e437dd61d0bb80
yaswanth <yaswanthgorre2@gmail.com>
2025-01-04 18:10:22 +0530

    react project dummy

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header(){
+    return(
+        <header>
+            <h1>my website</h1>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Service</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+            <hr></hr>
+        </header>
+    )
+}
+
+export default Header
